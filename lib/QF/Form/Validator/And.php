19f9283826af7a8162f0cc477714019ce2650043
tquensen <t.quensen@yeebase.com>
2011-09-19 10:28:39 +0200

    added validators

---
+++ lib/QF/Form/Validator/And.php
@@ -0,0 +1,28 @@
+<?php
+namespace QF\Form\Validator;
+
+class CombineAnd extends Validator
+{
+    protected $validators = array();
+
+    public function __construct($validators = array(), $options = array())
+	{
+        $this->validators = $validators;
+		$this->options = $options;
+
+        foreach ($this->validators as $validator) {
+            $validator->setElement($this->getElement());
+        }
+	}
+
+	public function validate($value)
+	{
+        foreach ($this->validators as $validator)
+        {
+            if (!$validator->validate($value)) {
+                return false;
+            }
+        }
+		return true;
+	}
+}
---
+++ lib/QF/Form/Validator/Exists.php
@@ -0,0 +1,38 @@
+<?php
+namespace QF\Form\Validator;
+
+class Exists extends Validator
+{
+	public function validate($value)
+	{
+		if ($this->getOption('values'))
+		{
+			return (in_array($value, $this->getOption('values')));
+		}
+
+        if ($this->getOption('model') && $this->getOption('property')) {
+			$model = $this->getOption('model');
+            if (is_string($model) && class_exists($model)) {
+                $model = new $model();
+            }
+            $property = $this->getOption('property');
+        } elseif ($element = $this->getElement()) {
+            $model = $element->getForm()->getModel();
+            $property = $element->getOption('modelProperty') ? $element->getOption('modelProperty') : $element->getName();
+        }
+        
+        if (!empty($model) && !empty($property) && method_exists((object) $model, 'getTable'))
+        {
+            try
+            {
+                return $model->getTable()->exist($property . ' = ?', $value);
+            }
+            catch (Exception $e)
+            {
+                return false;
+            }
+        }
+		
+		return false;
+	}
+}
