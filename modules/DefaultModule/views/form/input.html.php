3935f5ab59478573fbb31eb607ff841da95b3172
Thomas Quensen <thomas.quensen@t3n.de>
2013-01-10 13:50:39 +0100

    added view for input form element

--- modules/DefaultModule/views/form/input.html.php
+++ modules/DefaultModule/views/form/input.html.php
@@ -2,7 +2,7 @@
     <label
         for="<?php echo htmlspecialchars($element->getForm()->getName() . '__' . $element->getName()) ?>"><?php echo htmlspecialchars($element->label) ?><?php if ($element->required && $element->getForm()->getOption('requiredMark')): ?><?php echo '<span class="requiredMark">'.htmlspecialchars($element->getForm()->getOption('requiredMark')).'</span>'; ?><?php endif; ?></label>
     <?php endif; ?>
-    <input type="<?php echo htmlspecialchars($$element->getOption('type')); ?>"
+    <input type="<?php echo htmlspecialchars($element->getOption('type')); ?>"
            <?php if ($element->attributes): foreach ((array) $element->attributes as $attr => $attrValue): ?> <?php echo ' '.$attr.'="'.$attrValue.'"'; ?><?php endforeach; endif; ?>
            <?php if ($element->class): ?> class="<?php echo $element->class; ?>"<?php endif; ?>
            <?php if ($element->required): ?> required<?php endif; ?>
